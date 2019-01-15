69523816fc950af49f0ca6d990840f75f6aaf8d2
LAPTOP-OOVG6KOR\Piotr <pgardocki@interia.pl>
2019-01-15 21:07:59 +0100

    Create interface to communicate with links' data

---
+++ LinkShortenerAPI/DjangoAPI/shortener/appcode/core/links_db_access.py
@@ -0,0 +1,16 @@
+class LinksDBInterface:
+    def get_longlink_from_shortlink(self, shortlink, password=''): pass
+
+    def check_if_shortlink_is_in_db(self, shortlink): pass
+
+# methods for insertin links
+    def save_longlink_and_generate_shortlink(self, longlink, password=''): pass
+
+    def save_shortlink_and_longlink(self, shortlink, longlink, password=''): pass
+
+# methods for modyfying links
+    def change_shortlink(self, old_shortlink, new_shortlink): pass
+
+    def change_longlink(self, shortlink, old_longlink, new_longlink): pass
+
+    def change_password(self, shortlink, old_password, new_password): pass
