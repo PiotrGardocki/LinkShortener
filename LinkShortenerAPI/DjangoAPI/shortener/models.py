fd495362b5e15c1deed91d2cbd8107080374f534
LAPTOP-IUSC7P5E\Piotr <pgardocki@interia.pl>
2019-02-15 14:31:32 +0100

    Link Django's model for users in models.py

--- LinkShortenerAPI/DjangoAPI/shortener/models.py
+++ LinkShortenerAPI/DjangoAPI/shortener/models.py
@@ -1,4 +1,5 @@
 # from django.db import models
 # Create your models here.
 
-from shortener.appcode.django_db.django_model import LinksDB, AccessToDjangoLinksDB
+from shortener.appcode.django_db.django_links_model import LinksDB, AccessToDjangoLinksDB
+from shortener.appcode.django_db.django_users_model import UsersDB, AccessToDjangoUsersDB
