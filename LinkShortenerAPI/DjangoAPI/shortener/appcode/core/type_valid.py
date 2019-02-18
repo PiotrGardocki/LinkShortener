8fc30e2d20a6d15e01456e7edd27554b69ae51cf
LAPTOP-IUSC7P5E\Piotr <pgardocki@interia.pl>
2019-02-18 18:10:40 +0100

    Added validation in case of many possible types

--- LinkShortenerAPI/DjangoAPI/shortener/appcode/core/type_valid.py
+++ LinkShortenerAPI/DjangoAPI/shortener/appcode/core/type_valid.py
@@ -10,3 +10,20 @@ def validate_type(obj, correct_type, message=None):
             if not isinstance(message, str):
                 message = '[Incorrect message]'
             raise IncorrectType('Given type [%s], expected type [%s]; Message: %s' % (type(obj), correct_type, message))
+
+
+def validate_list_of_types(obj, correct_types, message=None):
+    validate_type(correct_types, list, 'correct_types must be list')
+
+    found_correct_type = False
+    for type_ in correct_types:
+        try:
+            validate_type(obj, type_)
+            found_correct_type = True
+            break
+        except IncorrectType:
+            pass
+
+    if not found_correct_type:
+        raise IncorrectType(('Given type [%s], expected types[' % type(obj)) + correct_types +
+                            (']; Message: %s' % message))
