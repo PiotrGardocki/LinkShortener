7a058e964a1d30b7a2ca91e5aedb9ece6d0d1d5c
LAPTOP-IUSC7P5E\Piotr <pgardocki@interia.pl>
2019-02-18 13:37:10 +0100

    Update of links interface

--- LinkShortenerAPI/DjangoAPI/shortener/appcode/core/links_db_access.py
+++ LinkShortenerAPI/DjangoAPI/shortener/appcode/core/links_db_access.py
@@ -1,16 +1,20 @@
 class LinksDBInterface:
-    def get_longlink_from_shortlink(self, shortlink, password=''): pass
+    def get_longlink_from_shortlink(self, shortlink, password=''):
+        pass
 
-    def check_if_shortlink_is_in_db(self, shortlink): pass
+    def check_if_shortlink_is_in_db(self, shortlink):
+        pass
 
-    def check_if_shortlink_has_password(self, shortlink): pass
+    def check_if_shortlink_has_password(self, shortlink):
+        pass
 
 # methods for inserting links
-    def save_shortlink_and_longlink(self, shortlink, longlink, password=''): pass
+    def save_shortlink_and_longlink(self, shortlink, longlink, password=''):
+        pass
 
 # methods for modyfying links
-#     def change_shortlink(self, old_shortlink, new_shortlink): pass
-#
-#     def change_longlink(self, shortlink, old_longlink, new_longlink): pass
-#
-#     def change_password(self, shortlink, old_password, new_password): pass
+    def change_shortlink(self, user_email, old_shortlink, new_shortlink): pass
+
+    def change_longlink(self, user_email, shortlink, old_longlink, new_longlink): pass
+
+    def change_password(self, user_email, shortlink, old_password, new_password): pass
