4c060fcc123af4ca9a4a535500d9f098c5074a3a
LAPTOP-IUSC7P5E\Piotr <pgardocki@interia.pl>
2019-02-18 18:11:23 +0100

    Added expiration time to Django links model

--- LinkShortenerAPI/DjangoAPI/shortener/appcode/django_models/links_model.py
+++ LinkShortenerAPI/DjangoAPI/shortener/appcode/django_models/links_model.py
@@ -7,4 +7,5 @@ class LinksDB(models.Model):
     shortlink = models.CharField(max_length=40, primary_key=True)
     longlink = models.URLField()   # db_index, validators
     password = models.CharField(max_length=30)
-    user = models.ForeignKey(UsersDB, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(UsersDB, on_delete=models.CASCADE, null=True)
+    expiration_date = models.DateField(null=True, db_index=True)
