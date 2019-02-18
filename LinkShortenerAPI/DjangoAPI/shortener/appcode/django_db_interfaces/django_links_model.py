9725fa0043d33f9cc7e7e37be35479adcb6e109e
LAPTOP-IUSC7P5E\Piotr <pgardocki@interia.pl>
2019-02-18 21:07:39 +0100

    General update for Django links interface

--- LinkShortenerAPI/DjangoAPI/shortener/appcode/django_db_interfaces/django_links_model.py
+++ LinkShortenerAPI/DjangoAPI/shortener/appcode/django_db_interfaces/django_links_model.py
@@ -1,46 +1,65 @@
 from django.db import IntegrityError
+from django.core.exceptions import ObjectDoesNotExist
 
 from shortener.appcode.core.links_interface import LinksInterface
 from shortener.appcode.core.db_errors import *
 from shortener.appcode.django_models.links_model import LinksDB
-from shortener.appcode.django_models.users_model import UsersDB
+from shortener.appcode.core.shortlink_data import ShortlinkData
+
+import random
 
 
 class AccessToDjangoLinksDB(LinksInterface):
     def get_longlink_from_shortlink(self, shortlink, password=''):
-        row = LinksDB.objects.filter(shortlink__exact=shortlink)
-        if not row.exists():
+        try:
+            row = LinksDB.objects.get(shortlink=shortlink)
+        except ObjectDoesNotExist:
             raise ShortLinkNotExists("shortlink (%s) does not exists" % shortlink)
-# TODO: refactor
-        row = row.get()
+
         if password != row.password:
             raise IncorrectPasswordForShortLink("password (%s) is incorrect for shortlink (%s)" % (shortlink, password))
 
         return row.longlink
 
-    def check_if_shortlink_is_in_db(self, shortlink):
-        return LinksDB.objects.filter(shortlink__exact=shortlink).exists()
-
-    def check_if_shortlink_has_password(self, shortlink):
-        row = LinksDB.objects.filter(shortlink__exact=shortlink)
-        if not row.exists():
-            raise ShortLinkNotExists("shortlink (%s) does not exists" % shortlink)
-
-        row = row.get()
-        return row.password != ''
-
-# methods for inserting links
-    def save_shortlink_and_longlink(self, shortlink, longlink, password=''):
-        # link = LinksDB(shortlink=shortlink, longlink=longlink, password=password)
-        # link.save()
+    def get_shortlink_data(self, shortlink):
         try:
-            LinksDB.objects.create(shortlink=shortlink, longlink=longlink, password=password)
-        except IntegrityError:
-            raise ShortLinkAlreadyTaken("shortlink (%s) already exists in database" % shortlink)
+            row = LinksDB.objects.get(shortlink=shortlink)
+        except ObjectDoesNotExist:
+            return ShortlinkData(False, False, False, None)
 
-# methods for modyfying links
-#     def change_shortlink(self, old_shortlink, new_shortlink): pass
-#
-#     def change_longlink(self, shortlink, old_longlink, new_longlink): pass
-#
-#     def change_password(self, shortlink, old_password, new_password): pass
+        has_password = True
+        if row.password == '':
+            has_password = False
+
+        is_from_user = True
+        if row.user is None:
+            is_from_user = False
+
+        expiration_date = row.expiration_date
+
+        return ShortlinkData(has_password, is_from_user, expiration_date)
+
+    def add_anonymous_shortlink(self, longlink, password=''):
+        shortlink_creation_succesful = False
+        shortlink = ''
+
+        while not shortlink_creation_succesful:
+            try:
+                shortlink = AccessToDjangoLinksDB.generate_random_shortlink()
+                LinksDB.objects.create(shortlink=shortlink, longlink=longlink, password=password)
+                shortlink_creation_succesful = True
+            except IntegrityError:
+                shortlink_creation_succesful = False
+
+        return shortlink
+
+    @staticmethod
+    def generate_random_shortlink():
+        letters = list('abcdefghijklmnopqrstuvwxyz0123456789')
+        shortlink = ''
+
+        for i in range(6):
+            shortlink += random.choice(letters)
+        # shortlink = random.choices(letters, k=6)
+
+        return shortlink
