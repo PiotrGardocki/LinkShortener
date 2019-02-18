49d493a03d166eb41f11f67167eeb071bf76163a
LAPTOP-IUSC7P5E\Piotr <pgardocki@interia.pl>
2019-02-18 20:56:41 +0100

    Fix of links interface

--- LinkShortenerAPI/DjangoAPI/shortener/appcode/core/links_interface.py
+++ LinkShortenerAPI/DjangoAPI/shortener/appcode/core/links_interface.py
@@ -2,19 +2,12 @@ class LinksInterface:
     def get_longlink_from_shortlink(self, shortlink, password=''):
         pass
 
-    def check_if_shortlink_is_in_db(self, shortlink):
+    def get_shortlink_data(self, shortlink):
         pass
 
-    def check_if_shortlink_has_password(self, shortlink):
+    def add_anonymous_shortlink(self, longlink, password=''):
         pass
 
-# methods for inserting links
-    def save_shortlink_and_longlink(self, shortlink, longlink, password=''):
+    @staticmethod
+    def generate_random_shortlink():
         pass
-
-# methods for modyfying links
-    def change_shortlink(self, user_email, old_shortlink, new_shortlink): pass
-
-    def change_longlink(self, user_email, shortlink, old_longlink, new_longlink): pass
-
-    def change_password(self, user_email, shortlink, old_password, new_password): pass
