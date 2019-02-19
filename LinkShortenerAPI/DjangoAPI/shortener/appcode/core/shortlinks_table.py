7d7463ab9cdae98ebfe08aafcebca1382ffdce0d
LAPTOP-IUSC7P5E\Piotr <pgardocki@interia.pl>
2019-02-19 17:51:53 +0100

    Added class to handle returnig shortlinks table of user

---
+++ LinkShortenerAPI/DjangoAPI/shortener/appcode/core/shortlinks_table.py
@@ -0,0 +1,31 @@
+class Shortlink:
+    def __init__(self, shortlink, longlink, password):
+        self.shortlink = shortlink
+        self.longlink = longlink
+        self.password = password
+
+    def __str__(self):
+        return '{"shortlink": "%s","longlink": "%s","password": "%s"}' % (self.shortlink, self.longlink, self.password)
+
+
+class ShortlinksTable:
+    def __init__(self):
+        self.table_of_shortlinks = []
+
+    def add_shortlink(self, shortlink, longlink, password):
+        self.table_of_shortlinks.append(Shortlink(shortlink, longlink, password))
+
+    def get_num_of_shortlinks(self):
+        return len(self.table_of_shortlinks)
+
+    def __str__(self):
+        string = '{['
+
+        links = []
+        for link in self.table_of_shortlinks:
+            links.append(str(link))
+
+        string += ','.join(links)
+
+        string += ']}'
+        return string
