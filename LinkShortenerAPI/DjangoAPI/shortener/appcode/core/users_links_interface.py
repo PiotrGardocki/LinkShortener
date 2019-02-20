37df6ae51ce8fb01c9d89d1b1dd096de2a183b64
LAPTOP-IUSC7P5E\Piotr <pgardocki@interia.pl>
2019-02-20 19:43:13 +0100

    Fixed misspelling and added method

--- LinkShortenerAPI/DjangoAPI/shortener/appcode/core/users_links_interface.py
+++ LinkShortenerAPI/DjangoAPI/shortener/appcode/core/users_links_interface.py
@@ -2,13 +2,16 @@ class UsersLinksInterface:
     def add_link(self, user_token, shortlink, longlink, password=''):
         pass
 
+    def add_random_link(self, user_token, longlink, password=''):
+        pass
+
     def delete_link(self, user_token, shortlink):
         pass
 
     def modify_shortlink(self, user_token, old_shortlink, new_shortlink):
         pass
 
-    def modify_longlink(self, user_token, shortlink, new_longlik):
+    def modify_longlink(self, user_token, shortlink, new_longlink):
         pass
 
     def modify_password(self, user_token, shortlink, new_password):
