148c8c1aa6bd9ae0c39cbdbd077023697d1687db
LAPTOP-IUSC7P5E\Piotr <pgardocki@interia.pl>
2019-03-10 12:59:37 +0100

    UsersLinksInterface deleted, methods moved to two other interfaces

--- LinkShortenerAPI/DjangoAPI/shortener/appcode/core/links_interface.py
+++ LinkShortenerAPI/DjangoAPI/shortener/appcode/core/links_interface.py
@@ -1,13 +1,24 @@
 class LinksInterface:
+    def add_link(self, longlink, link_password='', shortlink=None, user_token=None, expiration_date=None):
+        pass
+
+    def delete_link(self, user_token, shortlink):
+        pass
+
+    def modify_shortlink(self, user_token, old_shortlink, new_shortlink):
+        pass
+
+    def modify_longlink(self, user_token, shortlink, new_longlink):
+        pass
+
+    def modify_link_password(self, user_token, shortlink, new_password):
+        pass
+
     def get_longlink_from_shortlink(self, shortlink, password=''):
         pass
 
     def get_shortlink_data(self, shortlink):
         pass
 
-    def add_anonymous_shortlink(self, longlink, password=''):
-        pass
-
-    @staticmethod
-    def generate_random_shortlink():
+    def get_user_shortlinks_table(self, user_token):
         pass
--- LinkShortenerAPI/DjangoAPI/shortener/appcode/core/users_interface.py
+++ LinkShortenerAPI/DjangoAPI/shortener/appcode/core/users_interface.py
@@ -2,30 +2,23 @@ class UsersInterface:
     def create_user(self, email, password):
         pass
 
-    def delete_user(self, token):
-        pass
-
-    def change_user_password(self, token, new_password):
-        pass
-
     def log_user_in(self, email, password):
         pass
 
     def log_user_out(self, token):
         pass
 
-    def extend_token(self, token):
+    def delete_user(self, token):
         pass
 
-    def expire_token(self, token):
+    def change_user_password(self, token, new_password):
         pass
 
-    def get_user_for_token(self, token):
+    def change_user_email(self, token, new_email):
         pass
 
     def validate_token(self, token):
         pass
 
-    @staticmethod
-    def generate_token_for_user():
+    def refresh_token(self, token):
         pass
